fde5a99fcfbd83939fc8ff5264fe82d7df1f15a5
C442 <MayCh727@school.lu>
2023-01-07 13:18:02 +0100

    upload

--- Arcade_ordner/wordle.py
+++ Arcade_ordner/wordle.py
@@ -65,16 +65,20 @@ def wordle_guessing(word, word_size, word_interface, more_letters, lig):
                         more_letters.append(guess[index])
                     used_letters.append(guess[index])
                 if word[index] == guess[index]:
-                    word_interface[lig][index] = CGREENBG + BLACK + " " + guess[index] + " " + RESET + GREY
+                    word_interface[lig][index] = CGREENBG + \
+                        BLACK + " " + guess[index] + " " + RESET + GREY
                     more_letters.remove(guess[index])
             for index1 in range(word_size):
                 if guess[index1] in word and guess[index1] in more_letters and not word[index1] == guess[index1]:
-                    word_interface[lig][index1] = CYELLOWBG + BLACK + " " + guess[index1] + " " + RESET + GREY
+                    word_interface[lig][index1] = CYELLOWBG + \
+                        BLACK + " " + guess[index1] + " " + RESET + GREY
                     more_letters.remove(guess[index1])
                 elif not word[index1] == guess[index1]:
-                    word_interface[lig][index1] = GREY + " " + guess[index1] + " "
+                    word_interface[lig][index1] = GREY + \
+                        " " + guess[index1] + " "
             lig += 1
-            guessing = wordle_win_loose_detection(lig, word_size, word, guess, word_interface)
+            guessing = wordle_win_loose_detection(
+                lig, word_size, word, guess, word_interface)
         else:
             print(YELLOW + f"Word length must be {len(word)} letters!" + RESET)
 
@@ -83,7 +87,8 @@ def wordle_word_size(language):
     words = []
     request = True
     while request:
-        user_input = input(BLUE + "Select the number of letters in the word (3 to 6): ")
+        user_input = input(
+            BLUE + "Select the number of letters in the word (3 to 6): ")
         try:
             user_input = int(user_input)
         except ValueError:
@@ -92,31 +97,39 @@ def wordle_word_size(language):
             if 3 <= int(user_input) <= 6:
                 if user_input == 3:
                     if language == "German":
-                        words = ["auf", "aus", "das", "ufo", "ehe", "gab", "fee", "tee", "zug"]
+                        words = ["auf", "aus", "das", "ufo",
+                                 "ehe", "gab", "fee", "tee", "zug"]
                         break
                     elif language == "English":
-                        words = ["bit", "gap", "gas", "guy", "bro", "dog", "cat", "hut", "mad"]
+                        words = ["bit", "gap", "gas", "guy",
+                                 "bro", "dog", "cat", "hut", "mad"]
                         break
                 elif user_input == 4:
                     if language == "German":
-                        words = ["bahn", "auto", "sehr", "fein", "hund", "jahr", "jagd", "gans"]
+                        words = ["bahn", "auto", "sehr", "fein",
+                                 "hund", "jahr", "jagd", "gans"]
                         break
                     elif language == "English":
-                        words = ["area", "army", "baby", "card", "fish", "news", "wall", "bear"]
+                        words = ["area", "army", "baby", "card",
+                                 "fish", "news", "wall", "bear"]
                         break
                 elif user_input == 5:
                     if language == "German":
-                        words = ["hagel", "haase", "fahne", "tiere", "haare", "sache", "fahrt"]
+                        words = ["hagel", "haase", "fahne",
+                                 "tiere", "haare", "sache", "fahrt"]
                         break
                     elif language == "English":
-                        words = ["anger", "apple", "beach", "drink", "dress", "hotel", "steam"]
+                        words = ["anger", "apple", "beach",
+                                 "drink", "dress", "hotel", "steam"]
                         break
                 elif user_input == 6:
                     if language == "German":
-                        words = ["abflug", "achsel", "zahlen", "machen", "jaguar", "radler"]
+                        words = ["abflug", "achsel", "zahlen",
+                                 "machen", "jaguar", "radler"]
                         break
                     elif language == "English":
-                        words = ["better", "bottle", "damage", "eleven", "family", "garden"]
+                        words = ["better", "bottle", "damage",
+                                 "eleven", "family", "garden"]
                         break
             else:
                 print(RED + "Number of letters must be between 3 and 6!" + RESET)
@@ -129,7 +142,8 @@ def wordle_word_list():
     user_input = 0
     words = []
     while user_input != 1 and user_input != 2:
-        user_input = input(MAGENTA + "Please select the language of the words: ")
+        user_input = input(
+            MAGENTA + "Please select the language of the words: ")
         if user_input == "1":
             words = wordle_word_size("German")
             break
@@ -158,7 +172,8 @@ def wordle_game():
         wordle_guessing(word, word_size, word_interface, more_letters, lig)
         again = ""
         while again != "y" and again != "n":
-            print(BLUE + f"Wanna play again? {len(words)} words left to guess!")
+            print(
+                BLUE + f"Wanna play again? {len(words)} words left to guess!")
             again = input("Type y/n: " + RESET)
             if again == "n":
                 game = False
