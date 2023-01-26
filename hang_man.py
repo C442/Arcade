d8520dd713fb45e9222c27e9703be7cc31539371
BigblenHD <82868281+BigblenHD@users.noreply.github.com>
2023-01-26 12:01:16 +0100

    Add files via upload

---
+++ hang_man.py
@@ -0,0 +1,249 @@
+from mInput import *
+
+
+def hangman_title():
+    screen_title = r""" 
+ ___  ___  ________  ________   ________          _____ ______   ________  ________      
+|\  \|\  \|\   __  \|\   ___  \|\   ____\        |\   _ \  _   \|\   __  \|\   ___  \    
+\ \  \\\  \ \  \|\  \ \  \\ \  \ \  \___|        \ \  \\\__\ \  \ \  \|\  \ \  \\ \  \   
+ \ \   __  \ \   __  \ \  \\ \  \ \  \  ___       \ \  \\|__| \  \ \   __  \ \  \\ \  \  
+  \ \  \ \  \ \  \ \  \ \  \\ \  \ \  \|\  \       \ \  \    \ \  \ \  \ \  \ \  \\ \  \ 
+   \ \__\ \__\ \__\ \__\ \__\\ \__\ \_______\       \ \__\    \ \__\ \__\ \__\ \__\\ \__\
+    \|__|\|__|\|__|\|__|\|__| \|__|\|_______|        \|__|     \|__|\|__|\|__|\|__| \|__|                                 
+    """
+    print(GREENBRIGHT + screen_title)
+
+
+def hangman_frames(wrong_answer):  # different hangman frames to print out in console
+    hangman_frame = ""
+    if wrong_answer == 0:
+        hangman_frame = r"""
+        """
+    elif wrong_answer == 1:
+        hangman_frame = r"""
+        ____
+        |/   |
+        |   
+        |    
+        |    
+        |    
+        |
+        |_____
+        """
+    elif wrong_answer == 2:
+        hangman_frame = r"""
+         ____
+        |/   |
+        |   (_)
+        |    
+        |    
+        |    
+        |
+        |_____
+        """
+    elif wrong_answer == 3:
+        hangman_frame = r"""
+         ____
+        |/   |
+        |   (_)
+        |    |
+        |    |    
+        |    
+        |
+        |_____
+        """
+    elif wrong_answer == 4:
+        hangman_frame = r"""
+         ____
+        |/   |
+        |   (_)
+        |   \|
+        |    |
+        |    
+        |
+        |_____
+        """
+    elif wrong_answer == 5:
+        hangman_frame = r"""
+         ____
+        |/   |
+        |   (_)
+        |   \|/
+        |    |
+        |    
+        |
+        |_____
+        """
+    elif wrong_answer == 6:
+        hangman_frame = r"""
+         ____
+        |/   | 
+        |   (_)
+        |   \|/
+        |    |
+        |   / 
+        |
+        |_____
+        """
+    elif wrong_answer == 7:
+        hangman_frame = r"""
+         ____
+        |/   |
+        |   (_)
+        |   \|/
+        |    |
+        |   / \
+        |
+        |_____
+        """
+    elif wrong_answer == 8:
+        hangman_frame = r"""
+         ____
+        |/   |
+        |   (_)
+        |   /|\
+        |    |
+        |   | |
+        |
+        |_____
+        """
+    return hangman_frame
+
+
+def win_loose_detection(list1, list2, wrong_answer, round_counter, word):
+    if wrong_answer == 8:
+        print(BOLD + RED + "You lost!" + RESET)
+        print(BOLD + RED + "The word was: " + RESET + MAGENTA + word.upper())
+        game_state = False
+    elif list1 == list2:
+        print(BOLD + GREENBRIGHT +
+              f"You won and needed {round_counter} guess(es)!" + RESET)
+        game_state = False
+    else:
+        game_state = True
+    return game_state
+
+
+def individual_preset_words():  # Option 1: possibility to create your own set of words
+    words_preset = []
+    word_count = 1
+    action = True
+    print(BLUE + "Fix a preset of words you want to guess! (When finished enter 0)" + RESET)
+    while action:
+        user_input = input(MAGENTA + f"Enter {word_count}. word: ")
+        user_input = user_input.upper()
+        if user_input not in words_preset and user_input != "" and " " not in user_input:
+            words_preset.append(user_input.upper())
+            word_count += 1
+            if user_input == str(0):
+                if words_preset == ["0"]:
+                    print(RED + "You need to choose at least one word!" + RESET)
+                    words_preset.remove(user_input)
+                    word_count -= 1
+                else:
+                    words_preset = words_preset[:-1]
+                    return words_preset
+        else:
+            print(RED + "Word already used or no valid input given!")
+
+
+def preset_words():     # Option 2: lists of words to guess
+    player_input = "0"
+    word_set = []
+    print(BLUE + "Which language do you prefer?" + RESET)
+    print(GREEN + "1 " + RESET + "German")
+    print(GREEN + "2 " + RESET + "English")
+    while player_input != "1" and player_input != "2":
+        player_input = input(MAGENTA + "Choose your language: " + RESET)
+        if player_input == "1":
+            word_set = ["katze", "hund", "berg", "felsen", "wasser", "feuer", "erde", "stier", "kuh", "auto", "zug",
+                        "lastwagen", "baum", "elefant"]
+        elif player_input == "2":
+            word_set = ["cat", "dog", "mountain", "rock", "water", "fire", "earth", "taurus", "cow", "car", "train",
+                        "truck", "tree", "elephant"]
+        else:
+            print(RED + "Language does not exist!" + RESET)
+    return word_set
+
+
+def hangman_guessing(wrong_answer, word, interface, round_counter, letters, used_letters):
+    guessing = True
+    printout_letters = ""
+    print(YELLOW + "Game is starting...")
+    while guessing:
+        print(BOLD + CYAN + hangman_frames(wrong_answer))
+        for i in range(len(word)):
+            print(" ", end="")
+            print(RED + interface[i], end="")
+        print()
+        print(GREEN + "Used letters: ", end="")
+        if round_counter == 0:
+            print("/", end="")
+        print(printout_letters[:-2])
+        guessing = win_loose_detection(
+            interface, letters, wrong_answer, round_counter, word)
+        if guessing:
+            guess = input(MAGENTA + "Enter your guess: " + RESET)
+            if len(guess) == 1 and " " not in guess:
+                guess = guess.upper()
+                if guess in used_letters:
+                    print(YELLOW + "Letter already used!")
+                else:
+                    printout_letters += guess + ", "
+                    used_letters.append(guess)
+                    if guess in letters:
+                        for index in range(len(letters)):
+                            if letters[index] == guess:
+                                interface[index] = guess
+                    else:
+                        wrong_answer += 1
+                round_counter += 1
+            else:
+                print(RED + "Please enter one character!" + RESET)
+
+
+def hangman_menu():
+    user_input = "0"
+    words = ""
+    while user_input != "1" and user_input != "2":
+        print(CYAN + "1 " + RESET + "Prefixed set of words")
+        print(CYAN + "2 " + RESET + "Create your own set of words")
+        user_input = input(MAGENTA + "Please choose an option: " + RESET)
+        if user_input == "1":
+            words = preset_words()
+        elif user_input == "2":
+            words = individual_preset_words()
+        else:
+            print(RED + "Choosen option does not exist!" + RESET)
+    return words
+
+
+def hangman_game():
+    hangman_title()
+    words = hangman_menu()
+    game = True
+    while game:
+        word = choice(words)
+        words.remove(word)
+        wrong_answer = 0
+        letters = []
+        interface = len(word) * [H]
+        used_letters = []
+        round_counter = 0
+        for letter in word:
+            letters.append(letter.upper())
+        hangman_guessing(wrong_answer, word, interface,
+                         round_counter, letters, used_letters)
+        again = ""
+        while again != "n" and again != "y":
+            print(
+                BLUE + f"Wanna play again? {len(words)} words left to guess!")
+            again = input("Press y/n: ")
+            if again == "n":
+                game = False
+            elif again == "y":
+                if not words:
+                    print(RED + "No words left to guess!" + RESET)
+                    words = hangman_menu()
+            else:
+                print(RED + "Input not valid!" + RESET)
---
+++ wordle.py
@@ -0,0 +1,190 @@
+from mInput import *
+
+GREY = "\033[37m"
+BLACKBG = "\033[40m"
+
+
+def wordle_title():
+    print_title = BOLD + CYAN + r"""
+     .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+    | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+    | | _____  _____ | || |     ____     | || |  _______     | || |  ________    | || |   _____      | || |  _________   | |
+    | ||_   _||_   _|| || |   .'    `.   | || | |_   __ \    | || | |_   ___ `.  | || |  |_   _|     | || | |_   ___  |  | |
+    | |  | | /\ | |  | || |  /  .--.  \  | || |   | |__) |   | || |   | |   `. \ | || |    | |       | || |   | |_  \_|  | |
+    | |  | |/  \| |  | || |  | |    | |  | || |   |  __ /    | || |   | |    | | | || |    | |   _   | || |   |  _|  _   | |
+    | |  |   /\   |  | || |  \  `--'  /  | || |  _| |  \ \_  | || |  _| |___.' / | || |   _| |__/ |  | || |  _| |___/ |  | |
+    | |  |__/  \__|  | || |   `.____.'   | || | |____| |___| | || | |________.'  | || |  |________|  | || | |_________|  | |
+    | |              | || |              | || |              | || |              | || |              | || |              | |
+    | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+     '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'     
+     """ + RESET
+    print(print_title)
+
+
+def interface_box(word_size, word_interface):
+    box_print_top = GREY + TL + 3 * H + TR
+    box_print_bot = GREY + BL + 3 * H + BR
+    for size in range(word_size):
+        for lig in range(3):
+            for col in range(word_size):
+                if lig == 0:
+                    print(box_print_top, end="")
+                elif lig == 1:
+                    print(V + word_interface[size][col] + V, end="")
+                elif lig == 2:
+                    print(box_print_bot, end="")
+            print()
+
+
+def wordle_win_loose_detection(lig, word_size, word, guess, word_interface):
+    state = True
+    if lig == word_size and not word == guess:
+        interface_box(word_size, word_interface)
+        print(RED + "You lost!")
+        print(f"The word was: " + MAGENTA + word + RESET)
+        state = False
+    elif word == guess:
+        interface_box(word_size, word_interface)
+        print(GREEN + "You won!")
+        state = False
+    return state
+
+
+def wordle_guessing(word, word_size, word_interface, more_letters, lig):
+    guessing = True
+    while guessing:
+        interface_box(word_size, word_interface)
+        guess = input(MAGENTA + "Enter your guess: ")
+        guess = guess.upper()
+        if len(guess) == len(word):
+            used_letters = []
+            for index in range(word_size):      # first for loop: checks if a letter is at the right position
+                if guess[index] not in used_letters:
+                    letter_count = word.count(guess[index])     # count the number of each letter in the word to guess
+                    for _ in range(letter_count):
+                        more_letters.append(guess[index])
+                    used_letters.append(guess[index])
+                if word[index] == guess[index]:
+                    word_interface[lig][index] = CGREENBG + \
+                        BLACK + " " + guess[index] + " " + RESET + GREY
+                    more_letters.remove(guess[index])
+            for index1 in range(word_size):     # second for loop: checks letters occurrence in the word to guess
+                if guess[index1] in word and guess[index1] in more_letters and not word[index1] == guess[index1]:
+                    word_interface[lig][index1] = CYELLOWBG + \
+                        BLACK + " " + guess[index1] + " " + RESET + GREY
+                    more_letters.remove(guess[index1])
+                elif not word[index1] == guess[index1]:
+                    word_interface[lig][index1] = GREY + \
+                        " " + guess[index1] + " "   # if guess contains a letter more often than it occurs in the word to guess it gets grayed out
+            lig += 1
+            guessing = wordle_win_loose_detection(
+                lig, word_size, word, guess, word_interface)
+        else:
+            print(YELLOW + f"Word length must be {len(word)} letters!" + RESET)
+
+
+def wordle_word_size(language):
+    words = []
+    request = True
+    while request:
+        user_input = input(
+            BLUE + "Select the number of letters in the word (3 to 6): ")
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            print(RED + "Input has to be a number!")
+        else:
+            if 3 <= int(user_input) <= 6:
+                if user_input == 3:
+                    if language == "German":
+                        words = ["auf", "aus", "das", "ufo",
+                                 "ehe", "gab", "fee", "tee", "zug"]
+                        break
+                    elif language == "English":
+                        words = ["bit", "gap", "gas", "guy",
+                                 "bro", "dog", "cat", "hut", "mad"]
+                        break
+                elif user_input == 4:
+                    if language == "German":
+                        words = ["bahn", "auto", "sehr", "fein",
+                                 "hund", "jahr", "jagd", "gans"]
+                        break
+                    elif language == "English":
+                        words = ["area", "army", "baby", "card",
+                                 "fish", "news", "wall", "bear"]
+                        break
+                elif user_input == 5:
+                    if language == "German":
+                        words = ["hagel", "haase", "fahne",
+                                 "tiere", "haare", "sache", "fahrt"]
+                        break
+                    elif language == "English":
+                        words = ["anger", "apple", "beach",
+                                 "drink", "dress", "hotel", "steam"]
+                        break
+                elif user_input == 6:
+                    if language == "German":
+                        words = ["abflug", "achsel", "zahlen",
+                                 "machen", "jaguar", "radler"]
+                        break
+                    elif language == "English":
+                        words = ["better", "bottle", "damage",
+                                 "eleven", "family", "garden"]
+                        break
+            else:
+                print(RED + "Number of letters must be between 3 and 6!" + RESET)
+    return words
+
+
+def wordle_word_list():
+    print(GREEN + "1 " + WHITE + "German")
+    print(GREEN + "2 " + WHITE + "English")
+    user_input = 0
+    words = []
+    while user_input != 1 and user_input != 2:
+        user_input = input(
+            MAGENTA + "Please select the language of the words: ")
+        if user_input == "1":
+            words = wordle_word_size("German")
+            break
+        elif user_input == "2":
+            words = wordle_word_size("English")
+            break
+        else:
+            print(RED + "This language doesn't exist!")
+    return words
+
+
+def wordle_game():
+    wordle_title()
+    words = wordle_word_list()
+    game = True
+    while game:
+        word = choice(words)
+        words.remove(word)
+        word = word.upper()
+        word_size = len(word)
+        word_interface = []
+        for _ in range(word_size):
+            word_interface.append(word_size * [3 * " "])
+        lig = 0
+        more_letters = []
+        wordle_guessing(word, word_size, word_interface, more_letters, lig)
+        again = ""
+        while again != "y" and again != "n":
+            print(
+                BLUE + f"Wanna play again? {len(words)} words left to guess!")
+            again = input("Type y/n: " + RESET)
+            if again == "n":
+                game = False
+            elif again == "y":
+                if not words:
+                    print(RED + "No words left to guess!" + RESET)
+                    words = wordle_word_list()
+            else:
+                print(RED + "Input not valid!" + RESET)
+
+
+# main
+if __name__ == "__main__":
+    wordle_game()
